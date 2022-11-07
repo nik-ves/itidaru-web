8a706850f999c1dbfe71dae74dc2a838e42c1b71
Nikola Veselinović <nik-ves@hotmail.com>
2022-11-07 18:25:09 +0100

    useEffect cleanup function

--- src/components/todos/TodoList.js
+++ src/components/todos/TodoList.js
@@ -7,11 +7,15 @@ import Container from "../UI/Container";
 import { List, TodoMessage, Content, Title } from "./TodoList.styles";
 
 const TodoList = () => {
-  const { todos, getTodos } = useContext(TodoContext);
+  const { todos, getTodos, setTodos } = useContext(TodoContext);
   const { currentUser } = useContext(AuthContext);
 
   useEffect(() => {
     getTodos(currentUser.email);
+
+    return () => {
+      setTodos([]);
+    };
     // I want to keep dependencies array empty
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
